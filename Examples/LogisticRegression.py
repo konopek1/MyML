cb443c13bb030b9f4bf1782e2f79edbab943e93c
konopek1 <michal.konopka01@student.wat.edu.pl>
2020-12-18 10:01:55 +0100

    Add PCA

--- Examples/LogisticRegression.py
+++ Examples/LogisticRegression.py
@@ -1,27 +1,30 @@
 import numpy as np
 
 from Algorithms.SimpleGradient import SimpleGradient
+from PreProcessing.Normalize import normalize, normalize_by
 from Regression.CostFunctions import maximum_like_hood, d_maximum_like_hood
 from Regression.LogisticRegression import LogisticRegression
 
 # load data id,age,gender,height,weight,ap_hi,ap_lo,cholesterol,gluc,smoke,alco,active,cardio
 data = np.loadtxt('resources/cardio_train.csv', delimiter=',')
 
-
 # split
 xs, ys = np.c_[data[:30000, 3:5]], np.c_[data[:30000, 12]]
+xs, norms = normalize(xs)
+
 test_xs, test_ys = np.c_[data[6000:, 3:5]], np.c_[data[6000:, 12]]
 
 # Optimize
 gradient = SimpleGradient(cost_fn=maximum_like_hood, cost_d_fn=d_maximum_like_hood)
 
 # We get our parameters
-lr = LogisticRegression(optimizer=gradient,xs=xs,ys=ys)
+lr = LogisticRegression(optimizer=gradient, xs=xs, ys=ys)
 
-thetas, j_vals = lr.run(steps=9000, alpha=8)
+thetas, j_vals = lr.run(steps=100000, alpha=8)
 
 lr.plot()
 
+normalize_test_xs = normalize_by(test_xs, norms=norms)
 lr.test(test_xs=test_xs,test_ys=test_ys)
 
 # Predict
---
+++ PreProcessing/PCA.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def PCA(input: np.ndarray, dims) -> np.ndarray:
+    cor = COR(input)
+    eigen_vals, eigen_vecs = np.linalg.eig(cor)
+    eigen_vecs = sort_eigen(eigen_vals, eigen_vecs, dims=dims)
+
+    return input @ eigen_vecs.T
+
+
+def sort_eigen(eigen_vals, eigen_vecs, dims) -> np.ndarray:
+    indexed_vals = enumerate(eigen_vals)
+    sorted_vals = sorted(indexed_vals, key=lambda val: val[1], reverse=True)
+    choosen_indexes = list(map(lambda x: x[0], sorted_vals))[0:dims]
+
+    return eigen_vecs[choosen_indexes, :]
+
+
+def COR(input: np.ndarray) -> np.ndarray:
+    rows, cols = np.shape(input)
+    mean = np.mean(input, 0)
+    return np.dot(input.T, (input - mean)) / cols
